63548e3e8810e331f039458ce4ee1357aae080ad
Sven Aßmann <sven.assmann@lubico.biz>
2012-12-30 17:13:25 +0100

    file header improvements

--- decorators.py
+++ decorators.py
@@ -10,7 +10,7 @@ from django.utils.decorators import available_attrs
 __author__ = 'sassman <sven.assmann@lubico.biz>, http://djangosnippets.org/users/Scanner/'
 __version__ = "0.0.1"
 __license__ = "GNU Lesser General Public License"
-__package__ = ""
+__package__ = "gitana.decorators"
 
 def view_or_basicauth(view, request, test_func, realm = "", *args, **kwargs):
     """
--- exceptions.py
+++ exceptions.py
@@ -3,11 +3,13 @@
 __author__ = 'sassman <sven.assmann@lubico.biz>'
 __version__ = "0.0.1"
 __license__ = "GNU Lesser General Public License"
-__package__ = ""
+__package__ = "gitana.exceptions"
 
 class WrongGitCommandError(Exception):
     pass
 class NoValidRepository(Exception):
     pass
 class NoValidAccount(Exception):
+    pass
+class GitBackendError(Exception):
     pass
