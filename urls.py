a5b9111041f8ad3f6b35adcf82b7a594e06c76be
Sven Aßmann <sven.assmann@lubico.biz>
2012-12-30 17:14:14 +0100

    cleanup

--- urls.py
+++ urls.py
@@ -7,9 +7,10 @@ from gitana.decorators import logged_in_or_basicauth
 __author__ = 'sassman <sven.assmann@lubico.biz>'
 __version__ = "0.0.1"
 __license__ = "GNU Lesser General Public License"
-__package__ = ""
+__package__ = "gitana.urls"
 
 """ Urls that needs to be wrap
+    taken form the git-http-backend sources:
 
     {"GET", "/HEAD$", get_text_file},
     {"GET", "/info/refs$", get_info_refs},
@@ -24,45 +25,33 @@ __package__ = ""
     {"POST", "/git-receive-pack$", service_rpc}
 """
 
-"""
-url(r'^(?P<account_slug>[-\w]+)/(?P<repository_slug>[-\w]+)\.git/(?P<file_name>HEAD|objects/info/http-alternates|objects/info/alternates)$',
-    'gitana.views.get_text_file'
-),
-(r'^(?P<account_slug>[-\w]+)/(?P<repository_slug>[-\w]+)\.git/(?P<file_name>objects/[0-9a-f]{2}/[0-9a-f]{38})$',
-        'gitana.views.get_loose_object'),
-(r'^(?P<account_slug>[-\w]+)/(?P<repository_slug>[-\w]+)\.git/info/refs$', 'gitana.views.get_info_refs'),
-(r'^(?P<account_slug>[-\w]+)/(?P<repository_slug>[-\w]+)\.git/(?P<service>git\-upload\-pack|git\-receive\-pack)$', 'gitana.views.service_rpc'),
-"""
+urlpatterns = patterns('',
 
-gitana_url_prefix = r'^(?P<account_slug>[-\w]+)/(?P<repository_slug>[-\w]+)\.git/'
-
-urlpatterns = patterns(gitana_url_prefix,
-
-    url(GetTextFileView.url_pattern,
+    url(GetTextFileView.get_url_pattern(),
         logged_in_or_basicauth(GetTextFileView.as_view()),
         name='gitana_get_text_file'
     ),
-    url(GetInfoRefsView.url_pattern,
+    url(GetInfoRefsView.get_url_pattern(),
         logged_in_or_basicauth(GetInfoRefsView.as_view()),
         name='gitana_get_info_refs'
     ),
-    url(GetInfoPacksView.url_pattern,
+    url(GetInfoPacksView.get_url_pattern(),
         logged_in_or_basicauth(GetInfoPacksView.as_view()),
         name='gitana_get_info_packs'
     ),
-    url(GetLooseObjectView.url_pattern,
+    url(GetLooseObjectView.get_url_pattern(),
         logged_in_or_basicauth(GetLooseObjectView.as_view()),
         name='gitana_get_loose_object'
     ),
-    url(GetPackFileView.url_pattern,
+    url(GetPackFileView.get_url_pattern(),
         logged_in_or_basicauth(GetPackFileView.as_view()),
         name='gitana_get_pack_file'
     ),
-    url(GetIdxFileView.url_pattern,
+    url(GetIdxFileView.get_url_pattern(),
         logged_in_or_basicauth(GetIdxFileView.as_view()),
         name='gitana_get_idx_file'
     ),
-    url(ServiceRpcView.url_pattern,
+    url(ServiceRpcView.get_url_pattern(),
         logged_in_or_basicauth(ServiceRpcView.as_view()),
         name='gitana_service_rpc'
     ),
